0692ab7fa709fd5df21287fd6a286547ffa9945b
zanybird <saifur583509@gmail.com>
2023-11-08 16:18:11 +0600

    netlify

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://bejewelled-dragon-b28d12.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
