5d6dfe5ff7c1624181b37659391b75bdeb4123b8
zanybird <saifur583509@gmail.com>
2024-04-08 14:58:04 +0800

    fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://serene-tarsier-5ff748.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
